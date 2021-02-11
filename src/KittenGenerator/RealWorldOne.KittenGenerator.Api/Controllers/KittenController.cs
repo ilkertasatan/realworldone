46b2ea8ffecbc5053b665945f00ff8caf328cea7
ilkertasatan <itasatan@gmail.com>
2021-02-11 13:05:37 +0100

    Introduce unit tests for KittenGenerator

---
+++ src/KittenGenerator/RealWorldOne.KittenGenerator.Api/Controllers/KittenController.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RealWorldOne.KittenGenerator.Api.Controllers
+{
+    [Route("api/recipes")]
+    [Produces("application/json")]
+    [ApiController]
+    public class KittenController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<IActionResult> List()
+        {
+            return Ok();
+        }
+
+    }
+}
