ba1d658a57aa92ee61ceb8e1aa08f248d8283033
ilkertasatan <itasatan@gmail.com>
2021-02-12 11:50:46 +0100

    Introduce password value object

---
+++ src/UserManagement/RealWorldOne.UserManagement.Domain/Users/ValueObjects/Password.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace RealWorldOne.UserManagement.Domain.Users.ValueObjects
+{
+    public readonly struct Password : IEquatable<Password>
+    {
+        public Password(string value)
+        {
+            Value = value;
+        }
+
+        public string Value { get; }
+
+        public bool Equals(Password other)
+        {
+            return Value == other.Value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Password other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Value != null ? Value.GetHashCode() : 0);
+        }
+
+        public static bool operator ==(Password left, Password right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Password left, Password right)
+        {
+            return !(left == right);
+        }
+    }
+}
