8a9cadc59ddd08e92174f4e587025a845bc6735a
ilkertasatan <itasatan@gmail.com>
2021-02-12 18:21:26 +0100

    Add test for access token generator

---
+++ test/UnitTests/RealWorldOne.UserManagement.UnitTests/Infrastructure/Security/AccessTokenGeneratorShould.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using RealWorldOne.UserManagement.Infrastructure.Security;
+using Xunit;
+
+namespace RealWorldOne.UserManagement.UnitTests.Infrastructure.Security
+{
+    public class AccessTokenGeneratorShould
+    {
+        [Fact]
+        public void Generate_Token()
+        {
+            const int expectedExpiresIn = 300;
+            var sut = new AccessTokenGenerator();
+
+            var actualResult = sut.CreateAccessToken("aGVsbG9mcmVzaGdvX2JlX3Rlc3Q=", "sub", "iss", "aud");
+
+            actualResult.Token.Should().NotBeNull();
+            actualResult.ExpiresIn.Should().Be(expectedExpiresIn);
+        }
+    }
+}
