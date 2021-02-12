1e5b9fcdcd47fe1d2c86d7843f3342989b67da52
ilkertasatan <itasatan@gmail.com>
2021-02-12 11:54:26 +0100

    Introduce user domain and its factory class

---
+++ src/UserManagement/RealWorldOne.UserManagement.Domain/IUserFactory.cs
@@ -0,0 +1,10 @@
+﻿using RealWorldOne.UserManagement.Domain.Users;
+using RealWorldOne.UserManagement.Domain.Users.ValueObjects;
+
+namespace RealWorldOne.UserManagement.Domain
+{
+    public interface IUserFactory
+    {
+        User NewUser(UserId userId, Name name, Email email, Password password);
+    }
+}
---
+++ src/UserManagement/RealWorldOne.UserManagement.Domain/Users/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using RealWorldOne.UserManagement.Domain.Users.ValueObjects;
+
+namespace RealWorldOne.UserManagement.Domain.Users
+{
+    public class User : Entity<UserId>
+    {
+        public User(UserId userId, Name name, Email email, Password password)
+        {
+            Id = userId;
+            Name = name;
+            Email = email;
+            Password = password;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public Name Name { get; }
+        public Email Email { get; }
+        public Password Password { get; }
+        public DateTime CreatedAt { get; }
+    }
+}
