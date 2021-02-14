c449ee3e81a8cc93e0d12d2b89aa4f01f652792c
ilkertasatan <itasatan@gmail.com>
2021-02-14 20:08:48 +0100

    Add powershell scripts for user operations and remove sh script

---
+++ login-user.ps1
@@ -0,0 +1,21 @@
+﻿Write-host "Login user"  -ForegroundColor DarkGreen
+
+$Email = Read-Host "Enter email"
+$SecuredPassword = Read-Host "Enter password" -AsSecureString
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecuredPassword)
+$Password = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+
+$Body = @{
+    "email" = $Email
+    "password" = $Password
+}
+
+$Parameters = @{
+    Method = "POST"
+    Uri =  "http://localhost:5001/api/users/login"
+    Body = ($Body | ConvertTo-Json)
+    ContentType = "application/json"
+}
+Invoke-RestMethod @Parameters | ConvertTo-Json -Depth 10
+
+Write-host "Please copy access token to use kitten generator api"  -ForegroundColor DarkGreen
---
+++ new-user.ps1
@@ -0,0 +1,23 @@
+﻿Write-host "Create a new user"  -ForegroundColor DarkGreen
+
+$Name = Read-Host "Enter user name"
+$Email = Read-Host "Enter email"
+$SecuredPassword = Read-Host "Enter password" -AsSecureString
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecuredPassword)
+$Password = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+
+$Body = @{
+    "user_name" = $Name
+    "user_email" = $Email
+    "user_password" = $Password
+}
+
+$Parameters = @{
+    Method = "POST"
+    Uri =  "http://localhost:5001/api/users"
+    Body = ($Body | ConvertTo-Json)
+    ContentType = "application/json"
+}
+Invoke-RestMethod @Parameters | ConvertTo-Json -Depth 10
+
+Write-host "User has been created"  -ForegroundColor DarkGreen
